0ac6a12f126c7f7919ee05ea38543fa150aba64d
kawanet <u-suke@kawa.net>
2013-09-21 16:21:16 +0900

    Initial Commit

---
+++ KWImageArchive/DEMOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEMOAppDelegate.h
+//  KWImageArchive
+//
+//  Created by Yusuke Kawasaki on 2013/09/21.
+//  Copyright (c) 2013年 Kawanet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEMOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KWImageArchive/DEMOViewController.h
@@ -0,0 +1,13 @@
+//
+//  DEMOViewController.h
+//  KWImageArchive
+//
+//  Created by Yusuke Kawasaki on 2013/09/21.
+//  Copyright (c) 2013年 Kawanet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEMOViewController : UIViewController
+
+@end
